106bce6d433af274f42976c08595b7431ad5e666
rajaishwary <rajaishwary@gmail.com>
2017-03-27 01:28:58 +0530

    spinkit added with tags page

--- app/components/Tags.js
+++ app/components/Tags.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
 import {
     Text,
+    ScrollView,
+    View, 
+    StyleSheet
 } from 'react-native';
+import { connect } from 'react-redux';
 import TabBarContainer from '../common/TabBarContainer';
 import Header from '../common/Header';
-import { purple } from '../constants/color';
+import { purple, tilePurple } from '../constants/color';
 import { BLOG_NAME } from '../constants/config';
-import { topNotificationAreaHeight, heightWOtabBar, headerHeight } from '../constants/dimens';
-//<Header navigator={navigator} name={BLOG_NAME}/>
-export default class Tags extends Component {
+import { topNotificationAreaHeight, heightWOtabBar, headerHeight, width } from '../constants/dimens';
+
+class Tags extends Component {
+	constructor(props) {
+		super(props);
+        this.renderTags = this.renderTags.bind(this);
+		this.state = { 
+			requestingTags: true,
+			tags: null,
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const { data, requestingTags } = nextProps.tags;
+		this.setState({requestingTags, tags: data});
+	}
+
+    renderTags() {
+        if(this.state.requestingTags === true) {
+            return (
+                <View><Text>Getting data</Text></View>
+            );
+        } else {
+            return this.state.tags.map((tag, id) => (
+                <View key={id} style={styles.cardContainer}>
+                    <Text style={{textAlign: 'center', fontWeight: '600', fontSize: 18, padding: 5}}>#{tag.name}</Text>
+                </View>
+            ));
+        }
+    }
+
     render() {
         const { navigator } = this.props;
         return (
             <TabBarContainer>
                 <Header name={'Popular Tags'}/>
-
+                <View style={{top: headerHeight, width, height: heightWOtabBar - headerHeight}}>
+                	<ScrollView>
+                	  <View style={{alignItems: 'flex-start', flexDirection: 'row', flexWrap: 'wrap', margin: 5}}>
+                        {this.renderTags()}
+                	  </View>
+                	</ScrollView>
+                </View>
             </TabBarContainer>
         );
     }
 }
+
+function mapStateToProps(state) {
+  const { tags } = state;
+  return { tags };
+}
+
+export default connect(mapStateToProps)(Tags);
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: tilePurple,
+    justifyContent: 'center',
+    margin: 5,
+    borderRadius: 5,
+  }
+});
