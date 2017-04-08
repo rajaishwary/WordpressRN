0f8d8f4ff569b25966f0bc459569d5b3579d432f
rajaishwary <rajaishwary@gmail.com>
2017-04-08 19:33:18 +0530

    changed end point with json api

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import posts from './posts';
 import categories from './categories';
 import tags from './tags';
+import recent from './recent';
 
 const rootReducer = combineReducers({
     posts,
     categories,
-    tags
+    tags,
+    recent
 });
 
 export default rootReducer;
