5d128eee94d4e4d487ffd1068d8b36932f7dc47c
rajaishwary <rajaishwary@gmail.com>
2017-05-02 02:46:51 +0530

    fcm added to project for android

---
+++ ios/Pods/Target Support Files/Pods-WordpressRN-tvOS/Pods-WordpressRN-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordpressRN_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_WordpressRN_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-WordpressRNTests/Pods-WordpressRNTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WordpressRNTests : NSObject
+@end
+@implementation PodsDummy_Pods_WordpressRNTests
+@end
